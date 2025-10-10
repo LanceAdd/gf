1aa0f69957497f46994b59fe5725353b0a3d80df
shanyujie <1196661499@qq.com>
2025-10-10 10:13:35 +0800

    fix(gendao): 确保删除table

--- cmd/gf/internal/cmd/gendao/gendao_table.go
+++ cmd/gf/internal/cmd/gendao/gendao_table.go
@@ -26,11 +26,11 @@ import (
 
 // generateTable generates dao files for given tables.
 func generateTable(ctx context.Context, in CGenDaoInternalInput) {
+	dirPathTable := gfile.Join(in.Path, in.TablePath)
+	in.genItems.AppendDirPath(dirPathTable)
 	if !in.GenTable {
 		return
 	}
-	dirPathTable := gfile.Join(in.Path, in.TablePath)
-	in.genItems.AppendDirPath(dirPathTable)
 	for i := 0; i < len(in.TableNames); i++ {
 		var (
 			realTableName = in.TableNames[i]
